bf78d97c87effc9c61e13014d8adda97167728eb
FancyDogge <83734369+FancyDogge@users.noreply.github.com>
2022-04-03 01:42:20 +0300

    kek

--- MyTaskTracker/settings.py
+++ MyTaskTracker/settings.py
@@ -160,7 +160,7 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER ='djnagotestmailkek@gmail.com'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = '13579AGT'
+EMAIL_HOST_PASSWORD = 'noaqsbblivtwwbrq'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
